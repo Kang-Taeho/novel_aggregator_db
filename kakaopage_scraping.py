5f2206f397b0b1da9039a8035012b62bf541da05
Kang-Taeho <kthtrigger@gmail.com>
2022-12-31 20:31:14 +0900

    카카오페이지 웹스크래핑 최종_수정

--- kakaopage_scraping.py
+++ kakaopage_scraping.py
@@ -1,4 +1,5 @@
 import pymysql
+import time
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
@@ -20,20 +21,20 @@ try:
     with db.cursor() as cursor:
         cursor.execute("SELECT COUNT(id) FROM kakaopage_product")
         db_total_productNum = cursor.fetchone()[0]
-except(...) :
-    print("db sql문 SELECT COUNT(id) FROM kakaopage_product 오류")
-
+except(...):
+    print("sql문 SELECT COUNT(id) FROM kakaopage_product 오류")
+        
 #스크롤 끝까지 내리기
-# prev_height = driver.execute_script("return document. body.scrollHeight")
-# while True:
-#     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-#     time.sleep(1)
+prev_height = driver.execute_script("return document. body.scrollHeight")
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    time.sleep(1)
 
-#     current_height = driver.execute_script("return document. body.scrollHeight")
+    current_height = driver.execute_script("return document. body.scrollHeight")
     
-#     if current_height == prev_height :
-#         break
-#     prev_height = current_height
+    if current_height == prev_height :
+        break
+    prev_height = current_height
 
 #스크래핑
 #   작품id, visitor
@@ -44,7 +45,8 @@ socialList_id_visitor = []
 social_Number = soup.select_one('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div.flex.grow.flex-col > div.flex.grow.flex-col > div > div.flex.h-44pxr.w-full.flex-row.items-center.justify-between.bg-bg-a-10.px-15pxr > div.flex.h-full.flex-1.items-center.space-x-8pxr > span').get_text()
 social_Number = int(social_Number.replace(',','').replace('개',''))
 
-for num in range(1, social_Number - db_total_productNum + 1) :
+for num in range(1, social_Number + 1) :
+
     social = soup.select_one('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div.flex.grow.flex-col > div.flex.grow.flex-col > div > div.flex.grow.flex-col.py-10pxr.px-15pxr > div > div > div > div:nth-child({0}) > div > a > div'.format(num))
     
     raw_data_str =social['data-t-obj']
@@ -56,15 +58,27 @@ for num in range(1, social_Number - db_total_productNum + 1) :
     if '억' in social_Visitor : mul *= 100000000
     social_Visitor = social_Visitor.replace(',','').replace('만','').replace('억','')
     social_Visitor = float(social_Visitor) * mul
-    socialList_id_visitor.append((int(social_Id),int(social_Visitor)))
+
+    if num > (social_Number - db_total_productNum) : socialList_id_visitor.append((False,int(social_Id),int(social_Visitor)))
+    else : socialList_id_visitor.append((True,int(social_Id),int(social_Visitor)))
     mul = 1
     
 #   title, category, author, keyword, content
 try:
     with db.cursor() as cursor :
-        for social_Id, social_Visitor in socialList_id_visitor :
+        for new_work, social_Id, social_Visitor in socialList_id_visitor :
+            if new_work :
+                sql = """UPDATE kakaopage_product 
+                        SET visitor=%s
+                        WHERE id=%s 
+                """
+                val = (social_Visitor,social_Id)
+                cursor.execute(sql,val)
+                db.commit()
+                continue
+
             URL_Content = "https://page.kakao.com/content/" + str(social_Id) + "?tab_type=about"
-            
+
             driver_contnet = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
             driver_contnet.get(url=URL_Content)
              
