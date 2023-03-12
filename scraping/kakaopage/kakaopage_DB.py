5aeb69d92bfefebf03f9ff58cd9cf92c368841d7
Kang-Taeho <kthtrigger@gmail.com>
2023-03-12 20:55:36 +0900

    executemany 방식으로 수정

--- scraping/kakaopage/kakaopage_DB.py
+++ scraping/kakaopage/kakaopage_DB.py
@@ -22,123 +22,145 @@ class Novel_Info :
     Age_15gt = 0
     Completion_status = 0
     Number = 0
-    First_ep_date = ""
     Latest_ep_date = ""
     Visitor = 0
     Keyword = ""
-
+    
+db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
 with db.cursor() as cursor :
     sql = """SELECT title, author
                 FROM total_product"""
     cursor.execute(sql)
     INSERT_Tlist = list(cursor.fetchall())
 
-    sql = """SELECT kakao_id, title, author, img_url
+    sql = """SELECT kakao_id, img_url
+                FROM total_product
+                WHERE kakao_id IS NOT NULL AND img_url LIKE '%kakao.com%'"""
+    cursor.execute(sql)
+    UPDATE_img_Tlist = list(cursor.fetchall())
+
+    sql = """SELECT kakao_id, title, author
                 FROM total_product
                 WHERE kakao_id IS NOT NULL"""
     cursor.execute(sql)
-    UPDATE_Tlist = list(cursor.fetchall())
-    UPDATE_id_Tlist = list(map(lambda a : a[0],UPDATE_Tlist))
-    UPDATE_img_Tlist = list(map(lambda a : a[3],UPDATE_Tlist))
-    UPDATE_other_Tlist = list(map(lambda a : (a[0],a[1],a[2]),UPDATE_Tlist))
+    UPDATE_other_Tlist = list(cursor.fetchall())
+db.close()
 
 # 신작 DB INSERT 함수
-def DB_Insert() :
+def DB_Insert_Many(insert_novel_info_Tlist) :
+    """
+        insert_novel_info_Tlist 구성 요소 순서
+        (Id, Title, Author, Img_url, Description, Age_15gt, Completion_status, Number, Latest_ep_date, Visitor, Keyword) 
+    """
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
     try :
         with db.cursor() as cursor :
-            if Novel_Info.Age_15gt :
-                sql = """INSERT INTO kakaopage_15gt(id, age_15gt)
-                            VALUES(%s,%s)
-                """
-                val = (Novel_Info.Id, Novel_Info.Age_15gt)
-                cursor.execute(sql,val)
-                db.commit()
-                return
+            sql = """INSERT INTO kakaopage_product(id, age_15gt, completion_status, number, latest_ep_date, visitor, keyword)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+            val = list(map(lambda a: (a[0],a[5],a[6],a[7],a[8],a[9],a[10]),insert_novel_info_Tlist))
+            cursor.executemany(sql,val)
+            db.commit()
+    finally :
+        db.close()
 
-            else :
-                sql = """INSERT INTO kakaopage_product(id, age_15gt, completion_status, number, first_ep_date, latest_ep_date, visitor, keyword)
-                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""
-                val = (Novel_Info.Id, Novel_Info.Age_15gt, Novel_Info.Completion_status, Novel_Info.Number,
-                        Novel_Info.First_ep_date, Novel_Info.Latest_ep_date, Novel_Info.Visitor, Novel_Info.Keyword)
-                cursor.execute(sql,val)
-                db.commit()
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
+    try :
+        with db.cursor() as cursor :
+            sql = """UPDATE total_product
+                        SET kakao_id=%s
+                        WHERE title=%s AND author=%s"""
+            val = list(map(lambda a: (a[0],a[1],a[2]),
+                        list(filter(lambda b : (b[1],b[2]) in INSERT_Tlist ,insert_novel_info_Tlist))))
+            cursor.executemany(sql,val)
+            db.commit()
+    finally :
+        db.close()
 
-                if (Novel_Info.Title, Novel_Info.Author) in INSERT_Tlist :
-                    sql = """UPDATE total_product
-                                SET kakao_id=%s
-                                WHERE title=%s AND author=%s"""
-                    val = (Novel_Info.Id, Novel_Info.Title, Novel_Info.Author)
-                    cursor.execute(sql,val)
-
-                else :
-                    sql = """INSERT INTO total_product(kakao_id, title, author, img_url, description)
-                                VALUES(%s, %s, %s, %s, %s)"""
-                    val = (Novel_Info.Id, Novel_Info.Title, Novel_Info.Author, Novel_Info.Img_url, Novel_Info.Description)
-                    cursor.execute(sql,val)
-                db.commit()
-    except Exception :
-            db.close()
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
+    try :
+        with db.cursor() as cursor :    
+            sql = """INSERT INTO total_product(kakao_id, title, author, img_url, description)
+                        VALUES(%s, %s, %s, %s, %s)"""
+            val = list(map(lambda a: (a[0],a[1],a[2],a[3],a[4]),
+                        list(filter(lambda b : (b[1],b[2]) not in INSERT_Tlist ,insert_novel_info_Tlist))))
+            cursor.executemany(sql,val)
+            db.commit()
+    finally :
+        db.close()
     return 
 
 # 구작 DB UPDATE 함수
-def DB_Update() :
+def DB_Update_Many(update_novel_info_Tlist) :
+    """
+        update_novel_info_Tlist 구성 요소 순서
+        (Id, Title, Author, Img_url, Age_15gt, Completion_status, Number, Latest_ep_date, Visitor) 
+    """
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
     try :
         with db.cursor() as cursor :
-            if Novel_Info.Age_15gt : return
-            
             sql = """UPDATE kakaopage_product
                         SET pre_visitor=visitor
                         WHERE id=%s
             """
-            val = (Novel_Info.Id)
-            cursor.execute(sql,val)
+            val = list(map(lambda a: (a[0]),update_novel_info_Tlist))
+            cursor.executemany(sql,val)
             db.commit()
+    finally :
+            db.close()
 
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
+    try :
+        with db.cursor() as cursor :
             sql = """UPDATE kakaopage_product
-                        SET completion_status=%s, number=%s, latest_ep_date=%s, visitor=%s, keyword=%s 
+                        SET age_15gt=%s, completion_status=%s, number=%s, latest_ep_date=%s, visitor=%s 
                         WHERE id=%s"""
-            val = (Novel_Info.Completion_status, Novel_Info.Number, Novel_Info.Latest_ep_date, Novel_Info.Visitor, Novel_Info.Keyword, Novel_Info.Id)
-            cursor.execute(sql,val)
+            val = list(map(lambda a: (a[4],a[5],a[6],a[7],a[8],a[0]),update_novel_info_Tlist))
+            cursor.executemany(sql,val)
             db.commit()
+    finally :
+            db.close()
 
-            #img_url 변경시
-            if UPDATE_img_Tlist[UPDATE_id_Tlist.index(Novel_Info.Id)].find('kakao.com') :
-                 sql = """UPDATE total_product
-                            SET img_url=%s
-                            WHERE kakao_id=%s"""
-                 val = (Novel_Info.Img_url, Novel_Info.Id)
-                 cursor.execute(sql,val)
-                 db.commit()
-            
-            #title 또는 author 변경시
-            if (Novel_Info.Id, Novel_Info.Title, Novel_Info.Author) not in UPDATE_other_Tlist :
-                 sql = """UPDATE total_product
-                            SET title=%s, author=%s
-                            WHERE kakao_id=%s"""
-                 val = (Novel_Info.Title, Novel_Info.Author, Novel_Info.Id)
-                 cursor.execute(sql,val)
-                 db.commit()
-                
-    except Exception :
+    #img_url 변경시
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
+    try :
+        with db.cursor() as cursor :
+            sql = """UPDATE total_product
+                        SET img_url=%s
+                        WHERE kakao_id=%s"""
+            val = list(filter(lambda a : (a[1],a[0]) not in UPDATE_img_Tlist,
+                        list(filter(lambda b : (b[1]) in list(map(lambda c : (c[0]), UPDATE_img_Tlist)),
+                            list(map(lambda d : (d[3],d[0]),update_novel_info_Tlist)) )) ))
+            cursor.executemany(sql,val)
+            db.commit()
+    finally :
+            db.close()
+    
+     #title 또는 author 변경시
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
+    try :
+        with db.cursor() as cursor :
+            sql = """UPDATE total_product
+                        SET title=%s, author=%s
+                        WHERE kakao_id=%s"""
+            val = list(filter(lambda a : (a[0],a[1],a[2]) not in UPDATE_other_Tlist,
+                        list(map(lambda b : (b[1],b[2],b[0]), update_novel_info_Tlist)) ))
+            cursor.executemany(sql,val)
+            db.commit()  
+    finally :
             db.close()
     return 
 
 def DB_Delete() :
+    db = pymysql.connect(host=Test_DB.host,port=Test_DB.port,user=Test_DB.user,passwd=Test_DB.passwd,db=Test_DB.db,charset='utf8')
     try:
         with db.cursor() as cursor :
-            sql = """UPDATE total_product
-                        SET kakao_id=NULL
-                        WHERE kakao_id=%s"""
-            val = (Novel_Info.Id)
-            cursor.execute(sql,val)
-            db.commit()
-
             sql = """DELETE FROM kakao_product
                         WHERE id=%s"""
             val = (Novel_Info.Id)
             cursor.execute(sql,val)
             db.commit()
 
-    except Exception :
+    finally :
          db.close()
+    return
                
