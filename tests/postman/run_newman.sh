1f3cee770f3885027c5657c835193dce5254b5b0
taeho <kthtrigger@gmail.com>
2025-12-06 22:50:46 +0900

    postman/newman 테스트 추가 버전 (tests 일괄 테스트 진행 x)

--- tests/postman/run_newman.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-npx newman run "tests/postman//novel-aggregator.postman_collection.json" \
-  -e "tests/postman//local.postman_environment.json" \
-  --reporters cli,junit \
-  --reporter-junit-export "tests/postman/reports/newman-results.xml";
